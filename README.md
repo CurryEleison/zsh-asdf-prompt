316a608e89a57b5a3414914b83768a355f61d079
Thomas Petersen <petersen@temp.dk>
2022-02-01 14:42:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# asdf-prompt plugin for zsh/oh-my-zsh
+
+A `zsh` prompt for `asdf` users that displays information about your current tool versions.
+
+## Examples
+
+TODO
+
+## Requirements
+
+Currently only really tested with `oh-my-zsh`.
+
+## Install
+
+Run
+```zsh
+git clone https://github.com/CurryEleison/zsh-asdf-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+```
+
+to install the plugin into your `custom folder`. Activate the plugin in `~/.zshrc`:
+
+```zsh
+plugins=( [plugins...] zsh-asdf-prompt)
+```
+
+Add the prompt info in your theme with `$(asdf_prompt_info)`. E.g.
+```zsh
+PROMPT='%n@%m $(asdf_prompt_info)${(s:/:)PWD/#$HOME/~}%(!.#.>) '
+```
+
+## Environment variables in use
+- `ZSH_THEME_ASDF_PROMPT_PREFIX`: Prompt prefix. Default to [
+- `ZSH_THEME_ASDF_PROMPT_POSTFIX`: Prompt postfix. Default to ]
+- `ZSH_THEME_ASDF_PROMPT_FILTER`: Tools to show in prompt. `COMPACT`, `USER` or `ALL`.
+    - `ALL` shows all asdf tools 
+    - `USER` shows all that are not using `system` version
+    - `COMPACT` shows tools that are not system and not the default version for the user as defined by `~/.tool-versions`
+- `ZSH_THEME_ASDF_PROMPT_VERSION_DETAIL`: Version detail. `MINOR` or `PATCH`
+    - `MINOR`: Show major + minor version. Like "python: 3.10, nodejs: 17.4"
+    - `PATCH`: Include patch version. Like "python: 3.10.1, nodejs: 17.4.0"
+- `ZSH_THEME_ASDF_PROMPT_VERSION_RESOLUTION`: Show how version was resolved. `COMPACT` or `NONE`. Default is `NONE`
+    - `NONE`: Omit version resolution info
+    - `COMPACT`: Show how version was resolved by `asdf`
+        - "$" is from environment variables
+        - "~" from `.tool-versions` in home dir
+        - "." from `.tool-versions` in current dir
+        - "/" from `.tool-versions` in parent dir
---
+++ zsh-asdf-prompt.plugin.zsh
@@ -0,0 +1,46 @@
+
+function asdf_prompt_info() {
+  # If asdf isn't present nothing to do
+  (( ${+commands[asdf]} )) || return 0
+  local currenttools=`asdf current`
+  # Decide if we filter out system versions and/or versions defined as default for user
+  if [[ $ZSH_THEME_ASDF_PROMPT_FILTER != "ALL" ]]; then
+    currenttools=`echo $currenttools | grep -v ' system ' -`
+  fi
+  if [[ -z "${ZSH_THEME_ASDF_PROMPT_FILTER// }" || $ZSH_THEME_ASDF_PROMPT_FILTER == "COMPACT" ]]; then
+    currenttools=`echo $currenttools | grep -v "$HOME/.tool-versions" -`
+  fi
+
+  # Decide if anything is left to process and return if not.
+  [[ -z "${currenttools// }" ]] && return
+
+  local toolslist=`echo $currenttools | awk '{ print $1 }'`
+  local versionslist
+  # Decide if we do semi-major version (default) or full version info
+  if [[ $ZSH_THEME_ASDF_PROMPT_VERSION_DETAIL == "PATCH" ]]; then
+    versionslist=`echo $currenttools | awk '{ print $2 }' -`
+  elif [[ $ZSH_THEME_ASDF_PROMPT_VERSION_DETAIL == "MAJOR" ]]; then
+    versionslist=`echo $currenttools | awk '{ print $2 }' - | sed -E 's/([^\.]*)(\.[^\.]*)(\..*)/\1/g' - | sed 's/system/s/g' -`
+  else
+    versionslist=`echo $currenttools | awk '{ print $2 }' - | sed -E 's/([^\.]*)(\.[^\.]*)(\..*)/\1\2/g' - | sed 's/system/sys/g' -`
+  fi
+  # Decide if we want to print out origins or not (default)
+  local originslist
+  if [[ $ZSH_THEME_ASDF_PROMPT_VERSION_RESOLUTION == "COMPACT" ]]; then
+    originslist=`echo $currenttools | awk '{ print $3 }' | sed -E 's#ASDF_.*VERSION#\$#' | sed -E "s#$HOME\/*\.tool-versions#\~#" | sed -E "s#$PWD\/*\.tool-versions#\.#"  | sed -E "s#($HOME\/.+)#\/#" `
+  else
+    originslist=`echo $currenttools | awk '{ print ""}' -`
+  fi
+  # Paste columns together and scrunch up in a single line  
+  local reassembled=`paste  <(echo $toolslist) <(echo $versionslist) <(echo $originslist)`
+  local asdfsummary=`echo $reassembled | awk '{ print $1 ": " $2 $3 }' - | head -c -1 - | sed ':a;N;$!ba;s/\n/, /' -`
+
+  echo "${ZSH_THEME_ASDF_PROMPT_PREFIX-\{}$asdfsummary${ZSH_THEME_ASDF_PROMPT_POSTFIX-\}}"
+}
+
+# Default values for the appearance of the prompt.
+ZSH_THEME_ASDF_PROMPT_PREFIX="{"
+ZSH_THEME_ASDF_PROMPT_POSTFIX="} "
+ZSH_THEME_ASDF_PROMPT_FILTER="COMPACT"
+ZSH_THEME_ASDF_PROMPT_VERSION_DETAIL="MINOR"
+ZSH_THEME_ASDF_PROMPT_VERSION_RESOLUTION="NONE"
